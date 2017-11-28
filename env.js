5dc5347812cfd915fe1ba39784295887559bc8d9
William Le <wle8300@gmail.com>
2017-11-27 18:39:12 -0800

    asdf

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 module.exports = {
 	//NODE_ENV is implicitly defined via react-scripts@npm
-	backend: process.env.NODE_ENV === 'production' ? 'localhost:3003' : 'http://localhost:3003',
+	backend: process.env.NODE_ENV === 'production' ? 'https://api.nswatercooler.com:3000' : 'http://localhost:3000',
 	namespace: 'DAKKACLUB',
 	supportEmail: 'williamle8300@gmail.com'
 }
